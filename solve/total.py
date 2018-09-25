1d5e5bfe8b62fd6edec8f892578f457d8fc248ea
Sebastian <srverschoor@uwaterloo.ca>
2018-09-25 13:34:50 -0400

    total.py will count the total time spent benchmarking

---
+++ solve/total.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+total = 0
+with open('sat/cryptominisat5/karatsuba/timing.txt') as f:
+    for line in f:
+        if line.startswith('#'):
+            continue
+        total += float(line.split()[4])
+with open('sat/cryptominisat5/long/timing.txt') as f:
+    for line in f:
+        if line.startswith('#'):
+            continue
+        total += float(line.split()[4])
+with open('sat/maplecomsps/karatsuba/timing.txt') as f:
+    for line in f:
+        if line.startswith('#'):
+            continue
+        for word in line.split()[4:]:
+            total += float(word);
+with open('sat/maplecomsps/long/timing.txt') as f:
+    for line in f:
+        if line.startswith('#'):
+            continue
+        for word in line.split()[4:]:
+            total += float(word);
+
+with open('sat_min/timing.txt') as f:
+    for line in f:
+        if line.startswith('#'):
+            continue
+        total += float(line.split()[5]);
+
+with open('sat_multi/timing.txt') as f:
+    for line in f:
+        if line.startswith('#'):
+            continue
+        total += float(line.split()[2]);
+
+print(total)
